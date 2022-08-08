bc6abb2b205d18875d129b305ee618926944a6e3
eulerbolivar <euleragenda21@gmail.com>
2022-08-08 20:19:28 -0300

    dia 5 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1020 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1020 Exercicio.go
@@ -4,8 +4,44 @@ import (
     "fmt"
 )
 
+var diasVida int
+var anos int
+var meses int
+var dias int
 
+var contador, i int
 
 func main() {
-    fmt.Printf("Hello World!")
+    fmt.Scanf("%d", &diasVida)
+
+    //CONTA OS ANOS DE VIDA
+    for i = 0; i < diasVida; i++{
+        contador++
+        if contador == 365{
+            anos++
+            contador = 0
+        }
+    }
+
+    //CONTA OS MESES DE VIDA
+    diasVida = contador
+    contador = 0
+    for i = 0; i < diasVida; i++{
+        contador++
+        if contador == 30{
+            meses++
+            contador = 0
+        }
+    }
+
+    //CONTA OS DIAS DE VIDA
+    diasVida = contador
+    contador = 0
+    for i = 0; i < diasVida; i++{
+        dias++
+    }
+
+    fmt.Printf("%d ano(s)\n", anos)
+    fmt.Printf("%d mes(es)\n", meses)
+    fmt.Printf("%d dia(s)\n", dias)
 }
