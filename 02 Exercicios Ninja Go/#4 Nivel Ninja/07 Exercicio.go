c25b784b9b52a30e0ee6fffd1a600b26013a5004
eulerbolivar <euleragenda21@gmail.com>
2022-08-11 21:43:11 -0300

    Exercícios Feitos

--- 02 Exercicios Ninja Go/#4 Nivel Ninja/07 Exercicio.go
+++ 02 Exercicios Ninja Go/#4 Nivel Ninja/07 Exercicio.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main(){
-	fmt.Printf("teste")
+	ss := [][]string{
+				[]string{
+					"Marcio",
+					"Aurelio",
+					"Futebol",
+				},
+				[]string{
+					"Fernando",
+					"Cruzes",
+					"Beisebol",
+				},
+				[]string{
+					"Thiago",
+					"Zanatta",
+					"Natação",
+				},
+			}
+
+	for _, v := range ss{
+		fmt.Printf("%s\n", v[0])
+		for _, itens := range v{
+			fmt.Printf("\t %v\n", itens)
+		}
+	}
 }
--- 02 Exercicios Ninja Go/#4 Nivel Ninja/08 Exercicio.go
+++ 02 Exercicios Ninja Go/#4 Nivel Ninja/08 Exercicio.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main(){
-	fmt.Printf("teste")
+	
+	mepe := map[string][]string{
+			"Paula": []string{
+				"Música", "Café", "Teatro",
+			},
+			"Roberta": []string{
+				"Churrasco", "Cerveja", "Corrida",
+			},
+	}
+
+	for k, v := range mepe{
+		fmt.Printf("%v\n", k)
+		for i, h := range v{
+			fmt.Printf("%d - %v\n", i, h)
+		}
+		fmt.Printf("\n")
+	}
+	
 }
