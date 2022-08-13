f6f029309a1a721dba7e5386c1d70cffb8bb6633
eulerbolivar <euleragenda21@gmail.com>
2022-08-13 10:31:04 -0300

    Algoritmos Simples

--- 03 Exercicios Algoritmos/02 BubbleSort.go
+++ 03 Exercicios Algoritmos/02 BubbleSort.go
@@ -7,32 +7,23 @@ import (
 var i, j int
 
 func main() {
-	
+
 	//CRIA O ARRAY COM A QUANTIDADE DE ELEMENTOS, E INPUTA ESSES NÚMEROS
-	numeros := [3]int{}
+	numeros := [10]int{}
 	for i = 0; i < len(numeros); i++ {
 		fmt.Scanf("%d", &numeros[i])
 	}
 
-	//DÁ O OUTPUT COM OS 3 NÚMEROS ESCOLHIDOS
-	for i = 0; i < len(numeros); i++ {
-		fmt.Printf("%d\n", numeros[i])
-	}
-
-	fmt.Printf("\n")
+	//MOSTRA O ARRAY INICIAL
+	fmt.Printf("\n\nO = %d\n", numeros)
 
 	//FAZ A REORGANIZAÇÃO BUBBLE SORT
 	for i = 0; i < len(numeros)-1; i++ {
 		for j = 0; j < len(numeros)-i-1; j++ {
 			if numeros[j] > numeros[j+1] {
 				numeros[j], numeros[j+1] = numeros[j+1], numeros[j]
+				fmt.Printf("%d = %d\n", j, numeros)
 			}
 		}
 	}
-
-	//DÁ O OUTPUT COM OS 3 NÚMEROS REORGANIZADOS
-	for i = 0; i < len(numeros); i++ {
-		fmt.Printf("%d\n", numeros[i])
-	}
-
 }
