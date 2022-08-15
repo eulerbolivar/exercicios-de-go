87078971d9e08947110da18ad6d95c6ced46b8e1
eulerbolivar <euleragenda21@gmail.com>
2022-08-15 13:04:52 -0300

    dia 10 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1046 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1046 Exercicio.go
@@ -4,7 +4,17 @@ import (
 	"fmt"
 )
 
+var inicial, final int
+
 func main(){
-	fmt.Scanf()
+	fmt.Scanf("%d %d", &inicial, &final)
+
+	if final <= inicial{
+		final += 24
+	}
+
+	tempo := final - inicial
+
+	fmt.Printf("O JOGO DUROU %d HORA(S)\n", tempo)
 
 }
--- 04 Exercicios Beecrowd/01 Iniciante/1047 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1047 Exercicio.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 )
 
+var Hinicial, Minicial, Hfinal, Mfinal int
+var Htempo, Mtempo int
+
 func main(){
-	fmt.Scanf()
+	fmt.Scanf("%d %d %d %d", &Hinicial, &Minicial, &Hfinal, &Mfinal)
+
+	if Hfinal <= Hinicial{
+		Hfinal += 24
+	}
+
+	if Mfinal < Minicial{
+		Mfinal += 60
+	}
+
+	Htempo = Hfinal - Hinicial
+	Mtempo = Mfinal - Minicial
+
+	fmt.Printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", Htempo, Mtempo)
 
 }
