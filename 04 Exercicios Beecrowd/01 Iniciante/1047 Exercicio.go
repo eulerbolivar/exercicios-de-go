f5fe813453d455fffc10abc6417ea3d420d4a949
eulerbolivar <euleragenda21@gmail.com>
2022-08-15 22:38:55 -0300

    dia 10 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1047 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1047 Exercicio.go
@@ -10,12 +10,17 @@ var Htempo, Mtempo int
 func main(){
 	fmt.Scanf("%d %d %d %d", &Hinicial, &Minicial, &Hfinal, &Mfinal)
 
-	if Hfinal <= Hinicial{
+	if Hinicial == Hfinal && Minicial == Mfinal{
+		Hfinal += 24
+	}
+
+	if Hfinal < Hinicial{
 		Hfinal += 24
 	}
 
 	if Mfinal < Minicial{
 		Mfinal += 60
+		Hfinal -= 1
 	}
 
 	Htempo = Hfinal - Hinicial
