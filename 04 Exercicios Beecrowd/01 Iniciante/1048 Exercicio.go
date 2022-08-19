7362f014b2896d098d2788c3e7d56f812044125d
eulerbolivar <euleragenda21@gmail.com>
2022-08-18 23:32:25 -0300

    dia 13 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1048 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1048 Exercicio.go
@@ -1,10 +1,43 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main(){
-	fmt.Scanf()
+var Salario, NovoSalario, Reajuste float64
+var Porcentagem float64
 
+func main() {
+	fmt.Scanf("%f", &Salario)
+
+	switch {
+	case Salario > 0 && Salario <= 400:
+		Porcentagem = 15.0 / 100
+		NovoSalario = Salario + (Salario * Porcentagem)
+		Reajuste = NovoSalario - Salario
+
+	case Salario > 400 && Salario <= 800:
+		Porcentagem = 12.0 / 100
+		NovoSalario = Salario + (Salario * Porcentagem)
+		Reajuste = NovoSalario - Salario
+
+	case Salario > 800 && Salario <= 1200:
+		Porcentagem = 10.0 / 100
+		NovoSalario = Salario + (Salario * Porcentagem)
+		Reajuste = NovoSalario - Salario
+
+	case Salario > 1200 && Salario <= 2000:
+		Porcentagem = 7.0 / 100
+		NovoSalario = Salario + (Salario * Porcentagem)
+		Reajuste = NovoSalario - Salario
+
+	case Salario > 2000:
+		Porcentagem = 4.0 / 100
+		NovoSalario = Salario + (Salario * Porcentagem)
+		Reajuste = NovoSalario - Salario
+	}
+
+	IntPorcent := int(Porcentagem * 100)
+
+	fmt.Printf("Novo salario: %.2f\n", NovoSalario)
+	fmt.Printf("Reajuste ganho: %.2f\n", Reajuste)
+	fmt.Printf("Em percentual: %d %%\n", IntPorcent)
 }
