8ad24fa71f2ff4fca320cb8a54f70afee2d37f08
eulerbolivar <euleragenda21@gmail.com>
2022-08-19 22:26:13 -0300

    dia 14 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1049 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1049 Exercicio.go
@@ -1,10 +1,48 @@
 package main
+import "fmt"
 
-import (
-	"fmt"
-)
+var a, b, c string 
 
-func main(){
-	fmt.Scanf()
+func main() {
+    fmt.Scanf("%s", &a)
+    fmt.Scanf("%s", &b)
+    fmt.Scanf("%s", &c)
+    
+    if a == "vertebrado"{
+        if b == "ave"{
+            if c == "carnivoro"{
+                fmt.Printf("aguia\n")
+            }
+            if c == "onivoro"{
+                fmt.Printf("pomba\n")
+            }
+            }
+        if b == "mamifero"{
+            if c == "onivoro"{
+                fmt.Printf("homem\n")
+            }
+            if c == "herbivoro"{
+                fmt.Printf("vaca\n")
+            }
+        }
+    }
 
+    if a == "invertebrado"{
+        if b == "inseto"{
+            if c == "hematofago"{
+                fmt.Printf("pulga\n")
+            }
+            if c == "herbivoro"{
+                fmt.Printf("lagarta\n")
+            }
+        }
+        if b == "anelideo"{
+            if c == "hematofago"{
+                fmt.Printf("sanguessuga\n")
+            }
+            if c == "onivoro"{
+                fmt.Printf("minhoca\n")
+            }
+        }
+    }
 }
