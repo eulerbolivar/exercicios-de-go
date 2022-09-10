2ba9ed2a2bb1ecadda7ea45c72fbe20e061773f0
Euler Oliveira <96848490+eulerbolivar@users.noreply.github.com>
2022-09-09 21:12:53 -0300

    Add files via upload
    dia 34 beecrowd

---
+++ 04 Exercicios Beecrowd/01 Iniciante/1117 Exercicio.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+var nota, somado float64
+var contador int
+var media float64 = 0
+
+func main() {
+    
+    for i := 0; i != 2;{
+        fmt.Scanf("%f", &nota)
+        
+        if nota < 0 || nota > 10{
+            fmt.Printf("nota invalida\n")
+        }else{
+            somado += nota
+            contador++
+            i++
+        }
+    }
+    media = somado / float64(contador)
+    
+    fmt.Printf("media = %.2f\n", media)
+}
