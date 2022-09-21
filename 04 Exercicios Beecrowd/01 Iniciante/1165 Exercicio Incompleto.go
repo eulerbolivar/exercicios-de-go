1fa999e8b961897b531305dc75e692cd3d8827b3
eulerbolivar <euleragenda21@gmail.com>
2022-09-20 22:50:47 -0300

    dia 42 beecrowd

---
+++ 04 Exercicios Beecrowd/01 Iniciante/1165 Exercicio Incompleto.go
@@ -0,0 +1,27 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+    
+    var num, qtd, PrimoAux int
+
+    fmt.Scanf("%d", &qtd)
+    for i := 0; i < qtd; i++{
+        fmt.Scanf("%d", &num)
+        for j := 1; j <= num; j++{
+            if num % j != 0{
+                PrimoAux++
+                fmt.Printf("o número %d dividido por %d resultou em: %.2f\n", num, j, float64(num/j))
+            } 
+        }
+        if PrimoAux == 2{
+            fmt.Printf("%d eh primo\n", num)
+            PrimoAux = 0
+        } else {
+            fmt.Printf("%d nao eh primo\n", num)
+        }  
+    }
+}
