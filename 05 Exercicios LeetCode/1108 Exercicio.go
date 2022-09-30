6ab22e32d17804d558525dab435ad582473e3285
eulerbolivar <euleragenda21@gmail.com>
2022-09-30 15:04:19 -0300

    dia 4 LeetCode

--- 05 Exercicios LeetCode/1108 Exercicio.go
+++ 05 Exercicios LeetCode/1108 Exercicio.go
@@ -2,44 +2,38 @@ package main
 
 import(
 	"fmt"
-	"strconv"
 	"strings"
 )
-func minPartitions(n string) int {
-	var err error
 
-	texto := strings.Split(n, "")
-	separados := make([]int, len(texto))
-	
-	// SEPARA E RETORNA OS DIGITOS SEPARADOS EM UMA ARRAY 
-	for i := 0; i < len(texto); i++{
-		separados[i], err = strconv.Atoi(texto[i])
+func defangIPaddr(address string) string {
+	var ans string 
+	texto := strings.Split(address, "")
+
+	// ENCONTRA E APENDA OS COLCHETES EM VOLTA DOS PONTOS
+    for i := 0; i < len(texto); i++{
+		if texto[i] == "."{
+			texto[i-1] += "["
+			texto[i] += "]"
+		} 
 	}
 
-	// VÁLVULA DE ESCAPE PRA POSSÍVEIS ERROS
-	if err != nil{
-		fmt.Printf("%d\n", err)
+	// TRANSFORMA DE VOLTA O SLICE EM STRING PARA ENVIAR NO RETURN
+	for i := 0; i < len(texto);i++{
+		ans += texto[i]
 	}
 
-	// EXPÕE TODOS OS DADOS ARMAZENADOS
-	fmt.Printf("aqui será mostrado digito por digito do slice\n")
-	for i := 0; i < len(texto); i++{
-		fmt.Printf("%d\n", separados[i])
-	}
-
-	fmt.Printf("o número é: %d e o tipo é: %T\n", separados, separados)
-	
-	ans := 5
 	return ans
 }
 
 func main(){
+	var endereco string
 
-	var entrada string
-	// RECEBE O TEXTO E TRANSFORMA ELE EM UM SLICE DE STRINGS
-	fmt.Scanf("%s", &entrada)
+	/*in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadString('\n')*/
 
-	resp := minPartitions(entrada)
-	
-	fmt.Printf("%d\n", resp)
+	fmt.Scanf("%s", &endereco)
+
+	resp := defangIPaddr(endereco)
+
+	fmt.Printf("\n%s\n", resp)
 }
