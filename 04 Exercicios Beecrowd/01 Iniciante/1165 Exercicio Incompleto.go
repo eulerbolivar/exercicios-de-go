73471759cfc5b0ecc647eeeb624a3be80c6b82e7
eulerbolivar <euleragenda21@gmail.com>
2022-10-06 15:51:55 -0300

    dia 51 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1165 Exercicio Incompleto.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1165 Exercicio Incompleto.go
@@ -7,14 +7,15 @@ import (
 func main() {
     
     var num, qtd, PrimoAux int
+
     //PERCORRE OS VALORES PARA IDENTIFICAR OS NÚMEROS PRIMOS
     fmt.Scanf("%d", &qtd)
     for i := 0; i < qtd; i++{
         fmt.Scanf("%d", &num)
         for j := 1; j <= num; j++{
-            if num % j != 0{
+            if num % j == 0{
                 PrimoAux++
-                fmt.Printf("o número %d dividido por %d resultou em: %.2f\n", num, j, float64(num/j))
+                //fmt.Printf("o número %d dividido por %d sobrou: %.2f e o PrimoAux vale: %d\n", num, j, float64(num%j), PrimoAux)
             } 
         }
         if PrimoAux == 2{
@@ -22,6 +23,7 @@ func main() {
             PrimoAux = 0
         } else {
             fmt.Printf("%d nao eh primo\n", num)
+            PrimoAux = 0
         }  
     }
 }
