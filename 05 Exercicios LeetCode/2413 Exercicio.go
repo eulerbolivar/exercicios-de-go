8fcc17ec58f40696f7860d7a81eabc7e5d0ee893
Euler Oliveira <euleragenda21@gmail.com>
2023-01-07 12:50:19 -0300

    dia 16 LeetCode

---
+++ 05 Exercicios LeetCode/2413 Exercicio.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+)
+
+// FUNÇÃO SOLICITADA PELO EXERCÍCIO
+func smallestEvenMultiple(n int) int {
+    
+	for i := 0; i != x; i++{
+		if n % i	
+	}
+
+	return ans
+}
+
+// ENTRADA PADRÃO PARA O USO DA FUNÇÃO
+func main(){
+
+	resp := smallestEvenMultiple(5)
+	fmt.Printf("%d\n", resp)
+
+}
