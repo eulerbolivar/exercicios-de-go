dc1b0a48c82b94aa6fa7a97e052f469c7eb08f2e
Euler Oliveira <euleragenda21@gmail.com>
2023-01-15 23:30:46 -0300

    dia 57 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1179 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1179 Exercicio.go
@@ -7,9 +7,9 @@ import (
 func main() {
 
 	var j, k int
-	var controle := make([]int, 15)
-	var vetor1 := make([]int, 5)
-	var vetor2 := make([]int, 5)
+	var controle = make([]int, 15)
+	var vetor1 = make([]int, 5)
+	var vetor2 = make([]int, 5)
 
 	for i := 0; i < 15; i++{
 		fmt.Scanf("%d", &controle[i])
@@ -17,18 +17,17 @@ func main() {
 
 	for i := 0; i < 15; i++{
 		if (controle[i] % 2) == 0{
-			if j != 4{
-				vetor1[j]
+			if j != 5{
+				vetor1[j] = controle[i]
 				fmt.Printf("par[%d] = %d\n", j, vetor1[j])
 				j++
 			} else {
 				j = 0
 			}
-			
-		} else if (controle[i] % 2) != 0{ 
-			if k != 4{
-			vetor1[j]
-			fmt.Printf("par[%d] = %d\n", k, vetor2[k])
+		} else { 
+			if k != 5{
+			vetor2[k] = controle[i]
+			fmt.Printf("impar[%d] = %d\n", k, vetor2[k])
 			k++
 		} else {
 			k = 0
