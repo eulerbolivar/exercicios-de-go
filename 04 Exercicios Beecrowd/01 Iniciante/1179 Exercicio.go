5c5d603e092d051320307e1fa2300882a7347719
Euler Oliveira <euleragenda21@gmail.com>
2023-01-15 23:12:03 -0300

    dia 57 beecrowd

--- 04 Exercicios Beecrowd/01 Iniciante/1179 Exercicio.go
+++ 04 Exercicios Beecrowd/01 Iniciante/1179 Exercicio.go
@@ -6,9 +6,12 @@ import (
  
 func main() {
 
-	var 
+	var vetor1 := make([]int, 5)
+	var vetor2 := make([]int, 5)
 	for i := 0; i < 15; i++{
-
+		for j := 0; j < 5; j++{
+			fmt.Scanf("%d", &vetor1[i])
+		}
 	}
 
 }
